7eb483b2c77b524ec74203d5826615b2410d1f0e
KarolWojtas <karol.wojtas.aim@gmail.com>
2018-08-10 23:30:32 +0200

    h

--- server.js
+++ server.js
@@ -2,12 +2,16 @@
 
 const express = require('express');
 const app = express();
-
+const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
 app.all('*',(req,res)=>{
 	res.status(200).sendFile(__dirname + '/dist/index.html')
 });
+app.get('/*', function(req,res) {
+    
+	res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+	});
 app.listen(process.env.PORT || 8080);
 
